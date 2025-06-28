bf57e011d2d6b0492b1c66940e56ad192fd8005e
mosesbejon <idontwantnumbersinmyname123@gmail.com>
2025-06-28 17:34:00 +0100

    added comments to the view

--- respire/src/view.tsx
+++ respire/src/view.tsx
@@ -9,13 +9,19 @@ export class View{
         controller.subscribeToCount(this)
     }
 
+    // declared here so that TS doesn't get mad at me:
     setNewCountFunction(count: number){
 
     }
 
+    // this function must be run before newCount for this preview to work
+    // can be made to not be the case in future versions
+    // one (potentially problematic but potentially elegant) way to do this would be to run the contents of App in the...
+    // constructor 😬
     App() {
         const [count, setCount] = useState(0)
 
+        // this is so the controller can set the count:
         this.setNewCountFunction = setCount
 
         return (
