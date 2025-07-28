705718675831e345f9cd1ddbbe1b9474b350693c
mosesbejon <idontwantnumbersinmyname123@gmail.com>
2025-07-28 18:46:28 +0100

    fixed dodgy event listeners by removing them

--- respire/src/global.d.ts
+++ respire/src/global.d.ts
@@ -5,5 +5,6 @@ declare global {
         youtubePlayer: YT.Player;
         youtubeInfoGrabber: YT.Player;
         infoGrabberSubscribers: Set<{ onError: (errorCode: number) => void, onStateChange: (state: number) => void }>;
+        playerSubscribers: Set<{ onError: (errorCode: number) => void, onStateChange: (state: number) => void }>;
     }
 }
